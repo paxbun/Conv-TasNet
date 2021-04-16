6f0faec442be0df5a2a866fab830d1a07e9bfa2a
Chanjung Kim <freiyer.paxbun@gmail.com>
2021-04-17 01:15:59 +0900

    Add SDR implementation

--- conv_tasnet/loss.py
+++ conv_tasnet/loss.py
@@ -20,3 +20,18 @@ class SiSNR(tf.keras.losses.Loss):
         result = 20 * tf.math.log(tf.norm(e_noise) /
                                   (tf.norm(s_target + self.epsilon) + self.epsilon)) / math.log(10)
         return result
+
+
+class SDR(tf.keras.losses.Loss):
+    """Implements the SDR function.
+
+    Attributes:
+        epsilon: A small constant for enumerical stability
+    """
+
+    def __init__(self, epsilon: float = 1e-10, **kwargs):
+        super(SDR, self).__init__(**kwargs)
+        self.epsilon = epsilon
+
+    def call(self, s, s_hat):
+        return 20 * tf.math.log(tf.norm(s_hat - s) / (tf.norm(s) + self.epsilon) + self.epsilon) / math.log(10)
